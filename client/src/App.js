7bee2f86bbfa7b6432491b49cbba1c84d2e32dc7
Sheila <s.hopper01110@gmail.com>
2019-01-21 17:50:39 -0500

    React-Router-Movies Part 2 MVP complete

--- client/src/App.js
+++ client/src/App.js
@@ -24,8 +24,7 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <Route exact path="/" component={MovieList}/>
-        <Route path="/movies.id" component={Movie}/>
-        <div>Replace this Div with your Routes</div>
+        <Route exact path="/movies/:id" component={Movie}/>
       </div>
     );
   }
